3eac2ce04f1b27fa340fe293078dbbe04d5874c7
jeymar <jsbibjeymar@itcom888.com>
2022-08-04 10:26:38 +0800

    init

---
+++ src/test/java/com/example/hrassistantapp/HrAssistantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hrassistantapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrAssistantAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
